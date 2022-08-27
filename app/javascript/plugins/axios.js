2b8f82e8222d1a5534dfc6d9e49a917ca5a4c983
tmbann <mtdann1201@gmail.com>
2022-08-27 15:43:53 +0900

    Fix: パスワードリセットのルーティングエラー修正（axios修正）

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
+
+const production = "https://bridesap.herokuapp.com/";
+const development = "http://127.0.0.1:3000";
+const url = process.env.NODE_ENV === "production" ? production : development;
+
 const axiosInstance = axios.create({ 
-  baseURL: 'api' 
+  baseURL: `${url}/api/` 
 })
 
 if (localStorage.auth_token) {
