4e9d3d525c4071129357f1ed64ebaef79c4aaff5
tmbann <mtdann1201@gmail.com>
2022-09-07 21:58:14 +0900

    Fix: 本番環境のURLを独自ドメインに変更

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const production = "https://bridesap.herokuapp.com/";
+const production = "https://www.bridesap.com";
 const development = "http://127.0.0.1:3000";
 const url = process.env.NODE_ENV === "production" ? production : development;
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://bridesap.herokuapp.com'
+    origins 'http://localhost:3000', 'https://www.bridesap.com'
 
     resource '*',
             headers: :any,
